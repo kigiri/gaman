add9c778ff6c2b55d6ca13715ba5e6e49f16fe86
Clement Denis <le.mikmac@gmail.com>
2017-02-26 02:05:54 +0100

    heyheyhey

--- scraper/limit.js
+++ scraper/limit.js
@@ -1,13 +1,13 @@
 const oops = require('izi/oops')
 const err = oops('limit_error')
 
-module.exports = (limit=200, count = 0) => Object.assign(() => {
+module.exports = (limit=200, count = 0, start) => Object.assign(() => {
   if (count++ > limit) {
     throw err()
   }
 }, {
   start: () => start,
-  reset: () => count = 0,
+  reset: id => (count = 0, start = id),
 })
 
 module.exports.error = err
